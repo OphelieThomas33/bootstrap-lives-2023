6336ddff6c13714b68492234bb235757d9ab5be6
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-03-29 11:32:54 +0200

    elements lesson

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 const listCategories = document.querySelector('.list-categories')
 const listArticles = document.querySelector('#allArticles')
-const category = '<div class="category"><a href="#">Ma catégorie</a></div>'
-const article = '<article class="col-12 col-md-6 col-xl-4"><div><img src="https://picsum.photos/300/150" alt="Image générée par Lorem Picsum"></div><h2>Article</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac commodo augue, nec consequat lacus. Duis placerat, mi quis cursus suscipit, est diam volutpat neque, vitae tincidunt nulla turpis sed ante. Fusce eleifend arcu pharetra augue semper imperdiet. Curabitur eget sagittis risus, quis pulvinar nisi. Suspendisse ut diam et mauris laoreet fringilla.</p></article>'
+const category = '<div class="category list-group-item"><a href="#">Ma catégorie</a></div>'
+const article = '<article class="col-12 col-md-6 col-xl-4 mb-3"><div class="card" style="width: 18rem;"><img src="https://picsum.photos/300/150" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Article</h5><p class="card-text">Lorem ipsum dolor sit amet, Curabitur eget sagittis risus, quis pulvinar nisi. Suspendisse ut diam et mauris laoreet fringilla.</p><a href="#" class="btn btn-primary">Lire la suite<i class="bi bi-plus"></i></a></div></div></article>'
 
 let htmlCategories = '';
 
 for(let i=0; i<6 ; i++) {
     htmlCategories += category
 }
-listCategories.innerHTML = htmlCategories;
+listCategories.innerHTML += htmlCategories;
 
 let htmlArticles ='';
 for(let i=0; i<12 ; i++) {
     htmlArticles += article
 }
-listArticles.innerHTML = htmlArticles;
+listArticles.innerHTML += htmlArticles;
